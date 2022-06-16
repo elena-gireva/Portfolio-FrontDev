ebf1ff43a8d512c27bf2f4d204f6f79c01d63d52
Elena Gireva <94042128+elena-gireva@users.noreply.github.com>
2022-06-16 17:39:34 +0300

    Create script_resume.js

---
+++ script_resume.js
@@ -0,0 +1,12 @@
+let page_preloader = document.getElementById("page");
+
+let resume = document.querySelector("#resume");
+
+setTimeout(function() {
+  page_preloader.style.display = "none";
+  document.body.style.backgroundColor = "#95d2f2";
+
+  document.body.style.backgroundImage = 'url("background3.webp")';
+  resume.classList.remove("visibleContent");
+}, 1800);
+
